118c07254614a262e82c23b2b4534b86d231f79a
cyric2020 <Jordyboy.grieve@gmail.com>
2021-11-07 22:54:16 +1000

    a

--- Scripts/users_generate.py
+++ Scripts/users_generate.py
@@ -13,18 +13,38 @@ first_names_female = ['Mary', 'Patricia', 'Jennifer', 'Linda', 'Elizabeth', 'Bar
 
 last_names = ['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Garcia', 'Miller', 'Davis']
 
-with open('./users.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
+# with open('./users.csv', 'w', newline='') as f:
+#     writer = csv.writer(f)
 
-    writer.writerow(['id', 'password', 'gender', 'name', 'age'])
+#     writer.writerow(['id', 'password', 'gender', 'name', 'age'])
 
-    for i in range(1, 100):
-        id = '0' + str(random.randint(11111, 99999))
-        password = passwords[random.randint(0, (len(passwords)-1))]
-        password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        gender = random.randint(0, 1)
-        first_name = first_names_male[random.randint(0, (len(first_names_male)-1))] if (gender == 0) else first_names_female[random.randint(0, (len(first_names_female)-1))]
-        last_name = last_names[random.randint(0, (len(last_names)-1))]
-        age = random.randint(13, 18)
+#     for i in range(1, 100):
+#         id = '0' + str(random.randint(11111, 99999))
+#         password = passwords[random.randint(0, (len(passwords)-1))]
+#         password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+#         gender = random.randint(0, 1)
+#         first_name = first_names_male[random.randint(0, (len(first_names_male)-1))] if (gender == 0) else first_names_female[random.randint(0, (len(first_names_female)-1))]
+#         last_name = last_names[random.randint(0, (len(last_names)-1))]
+#         age = random.randint(13, 18)
 
-        writer.writerow([id, password, gender, first_name + ' ' + last_name, age])
+#         writer.writerow([id, password, gender, first_name + ' ' + last_name, age])
+
+users = []
+
+for i in range(1000):
+    id = '0' + str(random.randint(11111, 99999))
+    gender = random.randint(0, 1)
+    first_name = first_names_male[random.randint(0, (len(first_names_male)-1))] if (gender == 0) else first_names_female[random.randint(0, (len(first_names_female)-1))]
+    last_name = last_names[random.randint(0, (len(last_names)-1))]
+
+    user = f'{first_name} {last_name}, {id}'
+    
+    if(user not in users):
+        users.append(user)
+        print('Added game: ' + user)
+
+with open('output.txt', 'w') as f:
+    games_str = str(users)
+    games_str = games_str.replace("'", '"')
+    f.write(games_str)
+print(users)
